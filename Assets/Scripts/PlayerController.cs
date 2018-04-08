1670da94c541474026b490ce21420b24f223877b
eSaniello Studios <esaniello.studios@gmail.com>
2018-04-08 00:34:24 -0300

    added player animations and partial shooting mecanic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,14 +9,20 @@ public class PlayerController : MonoBehaviour {
     public Transform grounded;
     public LayerMask groundLayer;
     private Rigidbody2D rb;
+    Animator anim;
     private bool isGrounded = false;
     float accelerationTimeAirbourne = .2f;
     float accelerationTimeGrounded = .1f;
     float velXSmoothing;
+    public GameObject bullet;
+    public Transform gunTip;
+    public float bulletSpeed;
 
-	void Start ()
+
+    void Start ()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 	}
 	
 	void FixedUpdate ()
@@ -27,9 +33,32 @@ public class PlayerController : MonoBehaviour {
 
         rb.velocity = new Vector2(Mathf.SmoothDamp(rb.velocity.x , targetVelX , ref velXSmoothing , (isGrounded) ? accelerationTimeGrounded : accelerationTimeAirbourne) , rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetKeyDown(KeyCode.C) && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce * Time.deltaTime);
         }
+
+        if (Input.GetKeyDown(KeyCode.C) && rb.velocity.y > 0)
+        {
+            anim.SetBool("Jump", true);
+        }
+        else if (isGrounded)
+        {
+            anim.SetBool("Jump", false);
+        }
+
+        if (!isGrounded)
+        {
+            anim.SetFloat("yVel", Mathf.Clamp(rb.velocity.y , -1.0f , 1.0f));
+        }
 	}
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.X))
+        {
+            GameObject projectile = Instantiate(bullet, gunTip.position, Quaternion.identity);
+            projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletSpeed * Time.deltaTime, 0);
+        }
+    }
 }
