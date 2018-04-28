8be5b0d868e275ef319c4f3484d7d149a3aeb907
esaniello_studios <esaniello.studios@gmail.com>
2018-04-28 17:24:39 -0300

    working on dynamic scene loading

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public GameObject bullet;
+    public int objectPoolCount;
+    List<GameObject> objectPool;
+
+    private void Awake()
+    {
+        objectPool = new List<GameObject>();
+
+        for(int i = 0; i < objectPoolCount; i++)
+        {
+            GameObject projectile = Instantiate<GameObject>(bullet);
+            projectile.gameObject.SetActive(false);
+            objectPool.Add(projectile);
+        }
+    }
+
+    public GameObject GetBullet()
+    {
+        for (int i = 0; i < objectPool.Count; i++)
+        {
+            if (!objectPool[i].activeInHierarchy)
+            {
+                return objectPool[i];
+            }
+        }
+        return null;
+    }
+}
---
+++ NORA/Assets/Scripts/PlayerSwitchHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class PlayerSwitchHandler : MonoBehaviour
+{
+    public GameObject playerOne;
+    public GameObject playerTwo;
+    public CinemachineVirtualCamera virtualCam;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (playerOne.GetComponent<PlayerController>().enabled)
+            {
+                playerOne.GetComponent<PlayerController>().enabled = false;
+                virtualCam.Follow = playerTwo.transform;
+                playerTwo.GetComponent<PlayerController>().enabled = true;
+            }
+            else if (playerTwo.GetComponent<PlayerController>().enabled)
+            {
+                playerTwo.GetComponent<PlayerController>().enabled = false;
+                virtualCam.Follow = playerOne.transform;
+                playerOne.GetComponent<PlayerController>().enabled = true;
+            }
+        }
+    }   
+}
