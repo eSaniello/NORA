44c465dbb136c5b47ab10274c305c46b330d70c8
esaniello_studios <esaniello.studios@gmail.com>
2018-05-03 18:27:32 -0300

    working on scene unloading bug

--- Assets/Scripts/SceneTrigger.cs
+++ Assets/Scripts/SceneTrigger.cs
@@ -1,27 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneTrigger : MonoBehaviour
 {
     public Object[] ScenesToLoad;
     public Object[] ScenesToUnload;
 
+    Scene[] activeScenes = null;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "PlayerOne")
         {
-            for(int i = 0; i < ScenesToLoad.Length; i++)
-            {
-                SceneHandler.instance.LoadScene(ScenesToLoad[i]);
+            StartCoroutine(DoLoadingStuff());
+        }
+    }
 
-                for (int t = 0; i < ScenesToUnload.Length; i++)
-                {
-                    SceneHandler.instance.UnloadScene(ScenesToUnload[t]);
-                }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "playerOne")
+        {
+            activeScenes = new Scene[SceneManager.sceneCount];
+
+            for (int t = 0; t < SceneManager.sceneCount; t++)
+            {
+                activeScenes[t] = SceneManager.GetSceneAt(t);
             }
-            
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "PlayerOne")
+        {
+            StartCoroutine(DoUnloadingStuff());
             Destroy(gameObject);
         }
+    }
+    
+
+    IEnumerator DoLoadingStuff()
+    {
+        for (int i = 0; i < ScenesToLoad.Length; i++)
+        {
+            SceneHandler.instance.LoadScene(ScenesToLoad[i]);
+        }
+
+        yield return new WaitForEndOfFrame();
+    }
+
+    IEnumerator DoUnloadingStuff()
+    {
+        for (int i = 0; i < ScenesToUnload.Length; i++)
+        {
+                if (SceneManager.Equals(ScenesToUnload[i], activeScenes[i]))
+                    SceneHandler.instance.UnloadScene(ScenesToUnload[i]);
+                else
+                    break;
+            
+        }
+
+        yield return new WaitForEndOfFrame();
     }
 }
