feb98d726a2ec85f05759455595321859cd334c3
esaniello <shaniel.samadhan.natin@gmail.com>
2018-10-07 01:57:28 -0300

    bash mechanic in progress

---
+++ Assets/Testing/Shooter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform shootingDirection;
+    public float firerate;
+    public float bulletSpeed;
+
+    private void Start()
+    {
+        InvokeRepeating("Shoot", firerate, firerate);
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        Vector3 dir = shootingDirection.position - transform.position;
+        bullet.GetComponent<Rigidbody2D>().velocity = dir * bulletSpeed;
+    }
+}
